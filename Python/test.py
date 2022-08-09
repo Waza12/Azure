ee2e1dfd102a5e20d4d16bbbb094a8d4c7437960
Waza12 <j010398@gmail.com>
2022-08-09 15:18:07 +0200

    t

--- Python/test.py
+++ Python/test.py
@@ -1,4 +1,6 @@
+from ipaddress import get_mixed_type_key
 import random
+import ipaddress
 
 testnumb = random.randint(1,100)
-print("Random number is:", testnumb)
+print("Random number is:", ipaddress._IPAddressBase()    ([1, "192.168.0.1/24", "192.168.0.2/24"]))
