bb5015078c70ca1f3a18a309857cfe6f9b6b78df
Waza12 <j010398@gmail.com>
2022-08-09 16:30:54 +0200

    te

--- Python/test.py
+++ Python/test.py
@@ -1,17 +1,12 @@
-from ipaddress import IPv4Network
-
-
-
-
-def main():
-    for addr in IPv4Network('192.168.0.0/24'):
-        print(addr)
-
-if __name__ == "__main__":
-    main()
-    print("Test: ",__name__)
-
-print("__name__ equals " + __name__)
-
-if __name__ == '__main__':
-    print("if-statement was executed")
+count = 0
+while True:
+    name = input('Gimme your name: ')
+    if name != 'lol':
+        print('Your name:',name)
+        continue
+    password = input('Gimme your passw: ')
+    if password != 'meh':
+        continue
+    else:
+        print("Welcum.")
+        break
